867072f1d884ecad892fdb07af28b784ec6cfbcc
ksj <tjdwnd751@naver.com>
2021-11-26 17:20:09 +0900

    add AdminPage
    관리자화면 추가
    테이블 css만 적용
    메인에서 관리자페이지 이동

---
+++ src/main/java/com/example/controller/ManagerController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManagerController {
+
+//	user security 기본 id : user / password : 콘솔 Using generated security password: ex)75f93c5c-c13c-4bcf-aa11-b9c47b0f899a
+	
+	@GetMapping("/manager")
+	public String manager() {
+		return "view/home/viewManagerTemplate";
+	}
+	
+}
