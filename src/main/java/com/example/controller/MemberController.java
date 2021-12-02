7a18130361605f803f9fde65e463edc6123ea3da
jollypyun <58629366+jollypyun@users.noreply.github.com>
2021-12-02 20:39:43 +0900

    3-2
    [임시 커밋]
    - CSS 적용을 위한 COMMIT

--- src/main/java/com/example/controller/MemberController.java
+++ src/main/java/com/example/controller/MemberController.java
@@ -53,15 +53,18 @@ public class MemberController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		model.addAttribute("managerContent", "view/member/memberList");
 		model.addAttribute("lst", lst);
 		model.addAttribute("paging", paging);
-		return "view/member/memberList";
+		return "view/home/viewManagerTemplate";
 	}
 
 	@PostMapping("/members")
 	@ResponseBody
 	public Object viewSearchList(@RequestParam("keyword") String keyword, @RequestParam("keyfield") String keyfield,
 			Model model, Criteria crt) {
+		log.info(keyfield);
+		log.info(keyword);
 		List<MemberVO> lst = null;
 		Paging paging = new Paging();
 		try {
@@ -82,9 +85,9 @@ public class MemberController {
 		MemberVO member = this.memberService.retrieveMemberByManager(no);
 		log.info(member.toString());
 		model.addAttribute("member", member);
-		model.addAttribute("managerContent", "/fragments/view/ManagerContent");
+		model.addAttribute("managerContent", "view/member/detailMemberByManager");
 		System.out.println(model);
-		return "view/member/detailMemberByManager";
+		return "view/home/viewManagerTemplate";
 	}
 
 	@PostMapping("/members/{no}")
@@ -108,7 +111,8 @@ public class MemberController {
 			e.printStackTrace();
 		}
 		model.addAttribute("grades", grades);
-		return "view/member/gradeList";
+		model.addAttribute("managerContent", "view/member/gradeList");
+		return "view/home/viewManagerTemplate";
 	}
 
 	@PostMapping("/grades")
