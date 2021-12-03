72684b3fd06330b02c6def0886494d9464bb7e3e
pyn <mills_club@naver.com>
2021-12-03 15:25:36 +0900

    ㅁㄴㅇㄹ

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -105,6 +105,11 @@ public class ArticleController {
 		HttpSession session = req.getSession();
 		MemberVO member = (MemberVO) session.getAttribute("loginUser");
 		// bind
+		List<CategoryVO> categoryList = this.categoryService.retrieveCategoryBoardList();
+		model.addAttribute("categoryBoardList", categoryList);
+		CategoryVO categoryVo = new CategoryVO();
+		model.addAttribute("categoryVo", categoryVo);
+		
 		int gradeNo = member.getGradeNo();
 		log.info("writeForm get No::::" + gradeNo);
 		// bind
@@ -125,6 +130,11 @@ public class ArticleController {
 		HttpSession session = req.getSession();
 		MemberVO member = (MemberVO) session.getAttribute("loginUser");
 		int gradeNo = member.getGradeNo();
+		List<CategoryVO> categoryList = this.categoryService.retrieveCategoryBoardList();
+		model.addAttribute("categoryBoardList", categoryList);
+		CategoryVO categoryVo = new CategoryVO();
+		model.addAttribute("categoryVo", categoryVo);
+		
 		// bind
 		List<BoardVO> boardList = this.boardService.retrieveAllWriteBoard(gradeNo);
 		log.info("수정으로 넘어온 게시글 번호" +articleNo);
